d6106f2d0ef44dda0512ea42e65e591416fddcf5
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-01-16 15:01:55 +0700

    ded

--- js/app.js
+++ js/app.js
@@ -76,5 +76,5 @@ $(document).ready(function () {
 })
 
 //maen aos
-$("section p, .golongan .img, .data .img").attr({ "data-aos": "fade-up", "data-aos-delay": "100", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
+$("section p, .golongan .img").attr({ "data-aos": "fade-up", "data-aos-delay": "100", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
 $("section h3").attr({ "data-aos": "zoom-in", "data-aos-delay": "100", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
