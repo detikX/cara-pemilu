70630bc1a5b423e8486c28ec873872f77cc98354
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2024-01-26 10:45:07 +0700

    rfrf

--- js/app.js
+++ js/app.js
@@ -95,3 +95,57 @@ ScrollTrigger.create({
     scrub: true,
     markers: true
 })
+
+//countdown
+// Set the date we're counting down to
+var countDownDate = new Date("Feb 14, 2024 07:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  var jam = ("0" + hours).slice(-2);
+  var min = ("0" + minutes).slice(-2);
+  var hari = ("0" + days).slice(-2);
+  var tik = ("0" + seconds).slice(-2);
+
+  // Output the result in an element with id="demo"
+  //   document.getElementById("demo").innerHTML = days + " Hari " + hours + " Jam "
+  //   + minutes + " Menit " + seconds + " Detik ";
+
+  $(".day").html(`
+    ${hari} <small>Hari</small>
+`)
+  $(".ho").html(`
+    ${jam} <small>Jam</small>
+`)
+  $(".min").html(`
+    ${min} <small>Menit</small>
+`)
+  $(".sec").html(`
+    ${tik} <small>Detik</small>
+`)
+
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    // document.getElementById("demo").innerHTML = "EXPIRED";
+
+    $("#demo").html(
+      `
+        <div class="expired">Piala Dunia sedang berlangsung</div>
+      `
+    )
+  }
+}, 1000);
