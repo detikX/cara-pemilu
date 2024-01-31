7a0b256e0cd4ea75325be145e93af36f803c8294
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2024-01-31 13:05:17 +0700

    frf

--- js/app.js
+++ js/app.js
@@ -61,7 +61,8 @@ $('.kaca').slick({
             breakpoint: 480,
             settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                centerMode: true,
             }
         }
         // You can unslick at a given breakpoint now by adding:
