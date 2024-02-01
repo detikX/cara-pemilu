1ba1d47b813e7877696e0be0da6da0f962373150
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2024-02-01 11:04:55 +0700

    ded

--- js/app.js
+++ js/app.js
@@ -80,6 +80,8 @@ $(document).ready(function () {
 $("section p, .golongan .img").attr({ "data-aos": "fade-up", "data-aos-delay": "100", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
 $("section h3").attr({ "data-aos": "zoom-in", "data-aos-delay": "100", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" });
 
+$(".siapa .org").attr({ "data-aos": "fade-left", "data-aos-delay": "100", "data-aos-duration": "700", "data-aos-offset": "10", "data-aos-easing": "linear" });
+
 //gsap
 gsap.set(".photo:not(:first-child)", { opacity: 0, scale: 0.5 })
 
